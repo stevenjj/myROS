7c818846a8fb05d46b3ee4864fa83e5213f7cd8f
stevenjj <stevenjensj@gmail.com>
2015-09-23 21:19:59 -0500

    markers being published to RVIZ. now need to represent it as a smooth trajectory.

--- try_moveit/src/rosbag_visualize_marker_trajectory.cpp
+++ try_moveit/src/rosbag_visualize_marker_trajectory.cpp
@@ -44,13 +44,73 @@ void pub_marker(ros::Publisher &marker_pub){
     // Set the scale of the marker -- 1x1x1 here means 1m on a side
     marker.scale.x = 1.0;
     marker.scale.y = 1.0;
-    marker.scale.z = 1.0;
+    marker.scale.z = 0.5;
 
     // Set the color -- be sure to set alpha to something non-zero!
     marker.color.r = 0.0f;
     marker.color.g = 1.0f;
     marker.color.b = 0.0f;
     marker.color.a = 1.0;
+
+    marker.lifetime = ros::Duration();
+
+    // Publish the marker
+    while (marker_pub.getNumSubscribers() < 1)
+    {
+      if (!ros::ok())
+      {
+        ROS_WARN_ONCE("NOT OK!");
+        break;
+      }
+      ROS_WARN_ONCE("Please create a subscriber to the marker");
+      sleep(1);
+    }
+    marker_pub.publish(marker);
+
+
+
+}
+
+
+
+
+void pub_recorded_marker(ros::Publisher &marker_pub, visualization_msgs::Marker::ConstPtr &rosbag_marker ){
+     visualization_msgs::Marker marker;
+    uint32_t shape = visualization_msgs::Marker::CUBE;
+    // Set the frame ID and timestamp.  See the TF tutorials for information on these.
+    marker.header.frame_id = "/my_frame";
+    marker.header.stamp = ros::Time::now();
+
+    // Set the namespace and id for this marker.  This serves to create a unique ID
+    // Any marker sent with the same namespace and id will overwrite the old one
+    marker.ns = rosbag_marker->ns;//"basic_shapes";
+    marker.id = rosbag_marker->id;
+
+    // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
+    marker.type = rosbag_marker->type; //shape;
+
+    // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
+    marker.action = visualization_msgs::Marker::ADD;
+
+    // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
+    marker.pose.position.x = rosbag_marker->pose.position.x;//0;
+    marker.pose.position.y = rosbag_marker->pose.position.y;//0;
+    marker.pose.position.z = rosbag_marker->pose.position.z;//0;
+    marker.pose.orientation.x = rosbag_marker->pose.orientation.x;//0.0;
+    marker.pose.orientation.y = rosbag_marker->pose.orientation.y;//0.0;
+    marker.pose.orientation.z = rosbag_marker->pose.orientation.z;//0.0;
+    marker.pose.orientation.w = rosbag_marker->pose.orientation.w; //1.0;
+
+    // Set the scale of the marker -- 1x1x1 here means 1m on a side
+    marker.scale.x = rosbag_marker->scale.x; //1.0;
+    marker.scale.y = rosbag_marker->scale.y; //1.0;
+    marker.scale.z = rosbag_marker->scale.z; //0.5;
+
+    // Set the color -- be sure to set alpha to something non-zero!
+    marker.color.r = rosbag_marker->color.r; //0.0f;
+    marker.color.g = rosbag_marker->color.g; //1.0f;
+    marker.color.b = rosbag_marker->color.b; //0.0f;
+    marker.color.a = rosbag_marker->color.a; //1.0;
 
     marker.lifetime = ros::Duration();
 
@@ -101,6 +161,10 @@ int main(int argc, char **argv){
         std::cout << p->pose.orientation.y << std::endl;
         std::cout << p->pose.orientation.z << std::endl;
         std::cout << p->pose.orientation.w << std::endl;
+
+
+        pub_recorded_marker(rvizMarkerPub, p);
+        sleep(1);
     }
     ROS_INFO("Closing bag");
 
