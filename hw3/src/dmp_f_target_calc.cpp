02048ee11d282e2654e989b5c52f9c78fb34ef79
stevenjj <stevenjensj@gmail.com>
2015-10-07 22:18:19 -0500

    f_target_s implemented but not sure if it will work correctly.

--- hw3/src/dmp_f_target_calc.cpp
+++ hw3/src/dmp_f_target_calc.cpp
@@ -90,30 +90,45 @@ void pub_recorded_marker(ros::Publisher &marker_pub, visualization_msgs::Marker:
 
 
 // Calculates constant acceleration between two points x0, x1 and factors in velocity between them.
-float calc_acel(float x1, float xo, float vo, float dt){
+double calc_acel(double x1, double xo, double vo, double dt){
     return ( (x1 - xo - (vo*dt))/pow(dt,2) );
 }
 
 // Returns acceleration for x,y,z
-tf::Vector3 constant_a(tf::Vector3 &pos_next_i, tf::Vector3 &pos_i, tf::Vector3 &vel_i, float dt){
-    float a_x = calc_acel( pos_next_i.getX(), pos_i.getX(), vel_i.getX(), dt);
-    float a_y = calc_acel( pos_next_i.getY(), pos_i.getY(), vel_i.getY(), dt);
-    float a_z = calc_acel( pos_next_i.getZ(), pos_i.getZ(), vel_i.getZ(), dt);
+tf::Vector3 constant_a(tf::Vector3 &pos_next_i, tf::Vector3 &pos_i, tf::Vector3 &vel_i, double dt){
+    double a_x = calc_acel( pos_next_i.getX(), pos_i.getX(), vel_i.getX(), dt);
+    double a_y = calc_acel( pos_next_i.getY(), pos_i.getY(), vel_i.getY(), dt);
+    double a_z = calc_acel( pos_next_i.getZ(), pos_i.getZ(), vel_i.getZ(), dt);
     return tf::Vector3(a_x,a_y,a_z);
 }
 
 // Calculates velocity(i+1) from acceleration and prevoius velocity
-float calc_vel(float a_o, float v_o, float dt){
+double calc_vel(double a_o, double v_o, double dt){
     return a_o*dt + v_o;
 }
 
-tf::Vector3 vel_from_a(tf::Vector3 &acel_i, tf::Vector3 &vel_i, float dt){
-    float v_x = calc_vel( acel_i.getX(), vel_i.getX(), dt);
-    float v_y = calc_vel( acel_i.getY(), vel_i.getY(), dt);
-    float v_z = calc_vel( acel_i.getZ(), vel_i.getZ(), dt);
+tf::Vector3 vel_from_a(tf::Vector3 &acel_i, tf::Vector3 &vel_i, double dt){
+    double v_x = calc_vel( acel_i.getX(), vel_i.getX(), dt);
+    double v_y = calc_vel( acel_i.getY(), vel_i.getY(), dt);
+    double v_z = calc_vel( acel_i.getZ(), vel_i.getZ(), dt);
     return tf::Vector3(v_x, v_y, v_z);
 }
 
+// Calculates the forcing function f_target_s
+tf::Vector3 calc_f_target(double tau, tf::Vector3 &a_i, tf::Vector3 &v_i, tf::Vector3 &pos_i, tf::Vector3 &pos_init, 
+                    tf::Vector3 &pos_goal, double K, double D, double s){
+    double f_target_s_x = (tau*(a_i.getX() + D*v_i.getX())/K) - (pos_goal.getX() - pos_i.getX()) + (pos_goal.getX()-pos_init.getX())*s;
+    double f_target_s_y = (tau*(a_i.getY() + D*v_i.getY())/K) - (pos_goal.getY() - pos_i.getY()) + (pos_goal.getY()-pos_init.getY())*s;    
+    double f_target_s_z = (tau*(a_i.getZ() + D*v_i.getZ())/K) - (pos_goal.getZ() - pos_i.getZ()) + (pos_goal.getZ()-pos_init.getZ())*s;
+
+    return tf::Vector3(f_target_s_x, f_target_s_y, f_target_s_z);
+}
+
+std::vector<tf::Vector3> generate_waypoints(void){
+    std::vector<tf::Vector3> h;
+    h.push_back(tf::Vector3(0,0,0));
+    return h;
+}
 
 int main(int argc, char **argv){
     ros::init (argc, argv, "dmp_f_target_calc");
@@ -129,14 +144,18 @@ int main(int argc, char **argv){
     rosbag::View view(bag, rosbag::TopicQuery(topics));
 
 
-    std::vector< float > demo_t;
-    std::vector< tf::Vector3 > demo_pos;
-    std::vector< tf::Vector3 > demo_vel;
-    std::vector< tf::Vector3 > demo_acel;
+    // =======================================================================================================================================
+    // Parse rosbag
+    std::vector<double> demo_t;
+    std::vector<tf::Vector3> demo_pos;
+    std::vector<tf::Vector3> demo_vel;
+    std::vector<tf::Vector3> demo_acel;
 
     int start_time_s = 0;
+    double start_nsec = 0;
     int n_samples = 0;
 
+    // Go to the trajectory bag for the first time.
     foreach(rosbag::MessageInstance const m, view){       
         visualization_msgs::Marker::ConstPtr p = m.instantiate<visualization_msgs::Marker>();
         // Store marker x,y,z cartesian pose.        
@@ -146,42 +165,68 @@ int main(int argc, char **argv){
         // Store time variable;
         if (n_samples == 0) {
             start_time_s = p->header.stamp.sec; //Obtain first time stamp
+            start_nsec = (double) (p->header.stamp.nsec)/pow(10,9);    // Bring nanosec to seconds.     
         }
-        float sec = (float) (p->header.stamp.sec - start_time_s); //Subtract time stamp to get sensible seconds
-        float nsec = (float) (p->header.stamp.nsec)/pow(10,9);    // Bring nanosec to seconds.     
-        demo_t.push_back(sec + nsec); // Store the true time stamp
+        double sec = (double) (p->header.stamp.sec - start_time_s); // Subtract time stamp to get sensible seconds in int then convert to double
+        double nsec = (double) (p->header.stamp.nsec)/pow(10,9);    // Bring nanosec to seconds.     
+        demo_t.push_back(sec + nsec - start_nsec); // Store the true time stamp
 
         // Count total number of markers in the rosbag
         n_samples++; 
     }    
     //std::cout << n_samples << std::endl;
+    // =======================================================================================================================================
 
-    // Calculate derivatives
-
+    // =======================================================================================================================================
+    // Calculate velocity and acceleration
+        // Note some math functions:
+        //        std::cout << pow(10,2) << std::endl;
+        //        std::cout << log(1) << std::endl; 
+        //        std::cout << exp(1) << std::endl;
     // Initialize Velocity(0) to 0
     tf::Vector3 v_o(0,0,0);
     demo_vel.push_back(v_o);
-
     for (std::vector<int>::size_type i = 0; i < n_samples-1; ++i){
-        std::cout << i << std::endl;
-        std::cout << demo_pos[0].getX() << std::endl;
-
-        float dt = demo_t[i+1] - demo_t[i];
-        // Calculate acceleration(i) and add it to acceleration vector.
+        //std::cout << demo_pos[0].getX() << std::endl;
+        double dt = demo_t[i+1] - demo_t[i];
+        // Calculate constant acceleration(i) to get xo to x1 and add it to acceleration vector.
         demo_acel.push_back( constant_a(demo_pos[i+1], demo_pos[i], demo_vel[i], dt) );
         // Calculate velocity(i+1) and add it to velocity vector.
         demo_vel.push_back( vel_from_a(demo_acel[i], demo_vel[i], dt) );
+    }
+    // =======================================================================================================================================
 
-        std::cout << dt << std::endl;
+    // =======================================================================================================================================
+    // Calculate DMP Part 1: get f_target(s)
+    double K = 5.0;
+    double D = 0.2*sqrt(K);
+    double tau = 10; // in seconds
 
+
+    std::vector<double> phase_s;
+    std::vector<tf::Vector3> f_target_s;
+    tf::Vector3 demo_start = demo_pos[0]; //start position is set to be the first demo x,y,z position    
+    tf::Vector3 demo_goal = demo_pos[n_samples-1]; //goal is set to be the last demo x,y,z position
+    double tau_demo = demo_t[n_samples-2] - demo_t[0]; // duration of demo
+    double alpha = log(0.01); // selected so that s(t) = exp(-alpha/tau_demo)*t converges to 99% when t = tau_demo    
+
+    std::cout << tau_demo << std::endl;
+    std::cout << alpha << std::endl;
+    for (std::vector<int>::size_type i = 0; i < n_samples-1; ++i){
+        double s = exp(alpha/tau_demo * demo_t[i]); //Grab current time and find s
+        phase_s.push_back(s); // store phase variable s
+        // calculate f_target(s) 
+        f_target_s.push_back(  calc_f_target(tau_demo, demo_acel[i], demo_vel[i], demo_pos[i], demo_start, demo_goal, K, D, s) ); 
 
     }
+    // =======================================================================================================================================
+    // Calculate DMP part 2: Generate xyz waypoints
+    double tau_des = tau_demo; // Set duration of copying the trajectory
 
+    tf::Vector3 r_gripper_start_pos(0,0,0); //Modify this to pr2's starting arm position
+    tf::Vector3 r_gripper_goal_pos(0.75,0,0); //Modify this to pr2's starting arm position
 
-//        std::cout << pow(10,2) << std::endl;
-//        std::cout << log(1) << std::endl;
-//        std::cout << exp(1) << std::endl;
-
+    std::vector<tf::Vector3> xyz_waypoints = generate_waypoints();//generate_waypoints(K, D, tau_des, r_gripper_start_pos, r_gripper_goal_pos, f_target_s); 
 
 
 
